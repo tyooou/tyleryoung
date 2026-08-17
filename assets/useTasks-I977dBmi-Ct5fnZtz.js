c8c96c0a0e62ff941f345134e581b27c99934c81
Tyler <tylerhy@proton.me>
2026-08-17 16:39:45 +1200

    Updates

---
+++ assets/useTasks-I977dBmi-Ct5fnZtz.js
@@ -0,0 +1,1 @@
+import{r as n}from"./index-BAHD_W1V.js";import{c as r,aM as s}from"./StudioRoute-4bT5a5Uk.js";function i(){let e=r.c(1),o=n.useContext(s);if(!o){let t;return e[0]===Symbol.for("react.memo_cache_sentinel")?(t={activeDocument:null,setActiveDocument:a,data:[],isLoading:!1},e[0]=t):t=e[0],t}return o}function a(){return null}export{i as u};
